3e8b9144bfd4b9475e95497497dd76495ebac959
ihrimon <ihrimon147@gmail.com>
2026-01-12 21:08:53 +0600

    socket setup frontend

--- frontend/app/(main)/home.tsx
+++ frontend/app/(main)/home.tsx
@@ -3,17 +3,32 @@ import ScreenWrapper from '@/components/ScreenWrapper';
 import Typo from '@/components/Typo';
 import { colors } from '@/constants/theme';
 import { useAuth } from '@/contexts/authContext';
-import React from 'react';
+import { testSocket } from '@/socket/socket-events';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 
 const Home = () => {
-    const {user, signOut} = useAuth();
+  const { user, signOut } = useAuth();
 
-    console.log(user)
+  console.log(user);
 
-    const handleLogout = async () => {
-        await signOut();
-    }
+  useEffect(() => {
+    testSocket(testSocketCallbackHandler);
+    testSocket(null);
+
+    return () => {
+      testSocket(testSocketCallbackHandler, true);
+    };
+  }, []);
+
+  const testSocketCallbackHandler = (data: any) => {
+    console.log('Got response from testSocket event: ', data);
+  };
+
+  const handleLogout = async () => {
+    await signOut();
+  };
+  
   return (
     <ScreenWrapper>
       <Typo color={colors.white}>home</Typo>
